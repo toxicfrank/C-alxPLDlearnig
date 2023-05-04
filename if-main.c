710b0a527a6c20800f2b59fb1c773fa75825f37f
toxicfrank <101694828+toxicfrank@users.noreply.github.com>
2023-05-04 15:30:09 +0300

    practice

---
+++ if-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int userage;
+
+	printf("What is your age? ");
+
+	scanf("%d", &userage);
+
+	if (userage >= 18)
+	{
+		printf("You can drink alcohol if you like\n");
+	}
+	else if(userage >= 18 && userage <= 45)
+	{
+		printf("You mean you want to continue drinking");
+	}
+
+	else
+	{
+		printf("You cannot drink alcohol, Just take a soft drink");
+
+	}
+	return (0);
+}
